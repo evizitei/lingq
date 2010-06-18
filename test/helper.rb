a2949999118517e590ef01f440241daa436cd892
Ethan Vizitei <ethan.vizitei@gmail.com>
2010-06-18 13:52:46 -0500

    setup test helper for mocking web requests with WebMock
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
+require "bundler"
+Bundler.setup(:default,:test)
 require 'test/unit'
 require 'shoulda'
+require 'webmock/test_unit'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'lingq'
 
 class Test::Unit::TestCase
+  include WebMock
 end
