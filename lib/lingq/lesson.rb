df297a4e0342c2748fc4dad007365360e9ef0e8d
evizitei <ethan.vizitei@gmail.com>
2010-06-19 12:44:20 -0500

    let the lesson object load and cache it's member flashcards
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- lib/lingq/lesson.rb
+++ lib/lingq/lesson.rb
@@ -1,13 +1,19 @@
 module Lingq
   class Lesson
-    attr_reader :audio_url,:image_url,:id,:cards_count,:title,:language
-    def initialize(language_code,json)
+    attr_reader :audio_url,:image_url,:id,:cards_count,:title,:language,:lingq_client
+    def initialize(client,language_code,json)
+      @lingq_client = client
       @language = language_code
       @audio_url = json["audio_url"]
       @image_url = json["image_url"]
       @id = json["id"]
       @cards_count = json["cards_count"]
       @title = json["title"]
+    end
+    
+    def words
+      return @words if @words
+      @words = @lingq_client.words_for_lesson(self)
     end
   end
 end
--- test/test_lesson.rb
+++ test/test_lesson.rb
@@ -3,7 +3,9 @@ require 'helper'
 class TestLesson < Test::Unit::TestCase
   context "lesson" do
     setup do
-      @lesson = Lingq::Lesson.new("ru",{"audio_url"=>"http://m.lingq.com/media/_28/resources/contents/audio/2007/06/21/eating-out-3.mp3", 
+      Lingq::Client.any_instance.stubs(:load_languages!)
+      @client = Lingq::Client.new("apikey")
+      @lesson = Lingq::Lesson.new(@client,"ru",{"audio_url"=>"http://m.lingq.com/media/_28/resources/contents/audio/2007/06/21/eating-out-3.mp3", 
                                    "image_url"=>"http://m.lingq.com/media/_28/resources/contents/images/2008/08/26/Eating_Out_______.jpg", 
                                    "id"=> 21730, "cards_count"=> 31, "title"=> "Part 3" })
     end
@@ -30,6 +32,23 @@ class TestLesson < Test::Unit::TestCase
     
     should "cache language" do
       assert_equal "ru",@lesson.language
+    end
+    
+    should "cache the client that created it" do
+      assert_equal @client,@lesson.lingq_client
+    end
+    
+    context "loading words" do
+      setup do
+        @client.expects(:words_for_lesson).with(@lesson).returns([Lingq::Word.new("ru",{})])
+        @words = @lesson.words
+      end
+      
+      should "load words from client" do
+        assert_equal Lingq::Word,@words.first.class
+        assert_equal @lesson.language,@words.first.language
+        assert_equal 1,@words.size
+      end
     end
   end
 end
