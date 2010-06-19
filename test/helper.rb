2c1a9820684080fd7befe0f36021bbe6c79e1eeb
evizitei <ethan.vizitei@gmail.com>
2010-06-19 12:43:33 -0500

    require mocha for unit tests
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- test/helper.rb
+++ test/helper.rb
@@ -4,6 +4,7 @@ Bundler.setup(:default,:test)
 require 'test/unit'
 require 'shoulda'
 require 'webmock/test_unit'
+require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
